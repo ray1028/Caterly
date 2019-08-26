dd8456103e9fc605ba221d8f4e83941c22897e6e
Ricky <chhoukdean184@msn.com>
2019-08-26 14:10:13 +0000

    Change search query to show ID instead d of name

--- routes/01_home.js
+++ routes/01_home.js
@@ -20,7 +20,6 @@ module.exports = db => {
       if (userRes.rowCount !== 1) throw new Error("User is not found");
       if (categoriesRes.rowCount < 1) throw new Error("Categories not found");
 
-      console.log(categoriesRes.rows);
       res.render("home", {
         user: userRes.rows[0].first_name,
         categories: categoriesRes.rows
@@ -31,8 +30,20 @@ module.exports = db => {
   });
 
   router.post("/", (req, res) => {
-    let restaurant = req.body.search;
-    res.redirect(`/restaurants/${restaurant}`);
+
+    const queryRestaurantID = `
+    SELECT id FROM restaurants WHERE lower(name) LIKE lower($1) LIMIT 1`;
+
+    const restaurantName = [`%${req.body.search}%`];
+
+    db.query(queryRestaurantID, restaurantName).then(data => {
+      if (data.rowCount === 0) {
+        res.send("error");
+        console.log("DNE");
+      } else {
+        res.redirect(`/restaurants/${data.rows[0].id}`);
+      }
+    });
   });
   return router;
 };
