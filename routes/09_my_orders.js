2ae7724d5c9fb3d64cac255d785d18842a26e825
Ricky <chhoukdean184@msn.com>
2019-08-27 22:33:24 +0000

    Add my_orders functionality

--- routes/09_my_orders.js
+++ routes/09_my_orders.js
@@ -1,20 +1,36 @@
 const express = require("express");
 const router = express.Router();
+const strftime = require("strftime");
+
+const formatDate = function(epoch) {
+  return strftime("%F %T", new Date(parseInt(epoch)));
+};
 
 module.exports = db => {
   router.get("/", (req, res) => {
-    const templateVars = {};
+    const templateVars = { formatDate: formatDate };
     const queryStrHeader = `
     SELECT first_name, last_name FROM customers
       WHERE id = $1
     `;
-    const values = [req.session.user_id]
-    
-    db.query(queryStrHeader, values)
-      .then(data => {
-        templateVars.user = `${data.rows[0].first_name}`;
-      res.render("my_orders", templateVars);
+    const values = [req.session.user_id];
+
+    db.query(queryStrHeader, values).then(data => {
+      templateVars.user = `${data.rows[0].first_name}`;
+
+      const myOrdersQuery = `
+      SELECT customer_id, restaurant_id, created_at, pickup_time, order_total, restaurants.name as name FROM orders JOIN restaurants ON 
+      restaurants.id = restaurant_id JOIN customers ON customer_id = customers.id WHERE customers.id = $1 `;
+
+      db.query(myOrdersQuery, values).then(data2 => {
+        templateVars.orders = data2.rows;
+
+        console.log(data2);
+
+        res.render("my_orders", templateVars);
+      });
     });
   });
+
   return router;
 };
