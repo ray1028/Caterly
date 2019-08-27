17645623e37657364ff61d179b1163726983e11c
Jackson Fung <jacksonf21@hotmail.com>
2019-08-27 00:38:30 +0000

    fixed: dynamic pages not loading for restaurants

--- routes/02_login.js
+++ routes/02_login.js
@@ -2,6 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = db => {
+  router.get("/restaurant", (req, res) => {
+    const templateVars = {
+      error: false,
+      user: undefined
+    };
+    res.render("login-restaurant", templateVars);
+  });
+
   router.get("/", (req, res) => {
     const templateVars = {
       error: false,
